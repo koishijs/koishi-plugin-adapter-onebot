ca8e739d4e57f5018a3c7fdf28305475862d81b0
Shigma <shigma10826@gmail.com>
2024-09-17 22:24:14 +0800

    chore: bump version

--- src/types.ts
+++ src/types.ts
@@ -489,11 +489,6 @@ export interface Internal {
   getGroupRootFiles(group_id: id): Promise<GroupFileList>
   getGroupFilesByFolder(group_id: id, folder_id: string): Promise<GroupFileList>
   getGroupFileUrl(group_id: id, file_id: string, busid: number): Promise<string>
-  getGroupFileCount(group_id: id): Promise<number>
-  getGroupFileList(group_id: id, start_index: number, file_count: number, folder_id?: id): Promise<GroupFileList>
-  setGroupFileFolder(group_id: id, folder_name: string): Promise<GroupFolder>
-  delGroupFile(group_id: id, file_id: id): Promise<void>
-  delGroupFileFolder(group_id: id, folder_id: id): Promise<void>
   downloadFile(url: string, headers?: string | readonly string[], thread_count?: number): Promise<string>
   uploadPrivateFile(user_id: id, file: string, name: string): Promise<void>
   uploadGroupFile(group_id: id, file: string, name: string, folder?: string): Promise<void>
@@ -684,11 +679,6 @@ Internal.define('create_group_file_folder', 'group_id', 'folder_id', 'name')
 Internal.define('delete_group_folder', 'group_id', 'folder_id')
 Internal.define('delete_group_file', 'group_id', 'folder_id', 'file_id', 'busid')
 Internal.defineExtract('get_group_file_url', 'url', 'group_id', 'file_id', 'busid')
-Internal.defineExtract('get_group_file_count', 'group_id')
-Internal.defineExtract('get_group_file_list', 'group_id', 'start_index', 'file_count', 'folder_id')
-Internal.defineExtract('set_group_file_folder', 'group_id', 'folder_name')
-Internal.defineExtract('del_group_file', 'group_id', 'file_id')
-Internal.defineExtract('del_group_file_folder', 'group_id', 'folder_id')
 Internal.defineExtract('download_file', 'file', 'url', 'headers', 'thread_count')
 Internal.defineExtract('get_online_clients', 'clients', 'no_cache')
 Internal.defineExtract('check_url_safely', 'level', 'url')
